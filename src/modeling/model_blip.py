b06ccd5cc6ebb6b50976b0b877195e4d0b8e6028
Adrienne Deganutti <ad01355@surrey.ac.uk>
2024-12-01 23:37:32 +0000

    add vision projection layer

--- src/modeling/model_blip.py
+++ src/modeling/model_blip.py
@@ -4,15 +4,11 @@ from transformers import (
     AutoModelForCausalLM,
     AutoModelForSeq2SeqLM,
     Blip2Config,
-    Blip2VisionModel,
     Blip2ForConditionalGeneration,
     Blip2QFormerModel
 )
-from src.modeling.modeling_blip_2 import (
-    Blip2ForConditionalGeneration,
-    Blip2QFormerModel
-)
-from transformers.modeling_outputs import BaseModelOutputWithPooling
+from src.modeling.Blip2ForConditionalGeneration import Blip2ForConditionalGeneration
+from src.modeling.Blip2QFormer import Blip2QFormerModel
 
 
 class VideoBlipForConditionalGeneration(Blip2ForConditionalGeneration):
@@ -22,7 +18,7 @@ class VideoBlipForConditionalGeneration(Blip2ForConditionalGeneration):
         # self.vision_model
         super(Blip2ForConditionalGeneration, self).__init__(config)
 
-        self.vision_model = VideoBlipVisionModel(config.vision_config)
+        self.vision_projection = nn.Linear(in_features=1536, out_features=1408)
 
         self.query_tokens = nn.Parameter(
             torch.zeros(1, config.num_query_tokens, config.qformer_config.hidden_size)
@@ -40,84 +36,3 @@ class VideoBlipForConditionalGeneration(Blip2ForConditionalGeneration):
 
         # Initialize weights and apply final processing
         self.post_init()
-
-
-class VideoBlipVisionModel(Blip2VisionModel):
-    """A simple, augmented version of Blip2VisionModel to handle videos."""
-
-    def forward(
-        self,
-        pixel_values: torch.FloatTensor | None = None,
-        output_attentions: bool | None = None,
-        output_hidden_states: bool | None = None,
-        return_dict: bool | None = None,
-    ) -> tuple | BaseModelOutputWithPooling:
-        """Flatten `pixel_values` along the batch and time dimension, pass it
-        through the original vision model, then unflatten it back.
-
-        :param pixel_values: a tensor of shape (batch, channel, time, height, width)
-
-        :returns:
-            last_hidden_state: a tensor of shape (batch, time * seq_len, hidden_size)
-            pooler_output: a tensor of shape (batch, time, hidden_size)
-            hidden_states:
-                a tuple of tensors of shape (batch, time * seq_len, hidden_size),
-                one for the output of the embeddings + one for each layer
-            attentions:
-                a tuple of tensors of shape (batch, time, num_heads, seq_len, seq_len),
-                one for each layer
-        """
-        if pixel_values is None:
-            raise ValueError("You have to specify pixel_values")
-
-        batch, _, time, _, _ = pixel_values.size()
-
-        # flatten along the batch and time dimension to create a tensor of shape
-        # (batch * time, channel, height, width)
-        flat_pixel_values = pixel_values.permute(0, 2, 1, 3, 4).flatten(end_dim=1)
-
-        vision_outputs: BaseModelOutputWithPooling = super().forward(
-            pixel_values=flat_pixel_values,
-            output_attentions=output_attentions,
-            output_hidden_states=output_hidden_states,
-            return_dict=True,
-        )
-
-        # now restore the original dimensions
-        # vision_outputs.last_hidden_state is of shape
-        # (batch * time, seq_len, hidden_size)
-        seq_len = vision_outputs.last_hidden_state.size(1)
-        last_hidden_state = vision_outputs.last_hidden_state.view(
-            batch, time * seq_len, -1
-        )
-        # vision_outputs.pooler_output is of shape
-        # (batch * time, hidden_size)
-        pooler_output = vision_outputs.pooler_output.view(batch, time, -1)
-        # hidden_states is a tuple of tensors of shape
-        # (batch * time, seq_len, hidden_size)
-        hidden_states = (
-            tuple(
-                hidden.view(batch, time * seq_len, -1)
-                for hidden in vision_outputs.hidden_states
-            )
-            if vision_outputs.hidden_states is not None
-            else None
-        )
-        # attentions is a tuple of tensors of shape
-        # (batch * time, num_heads, seq_len, seq_len)
-        attentions = (
-            tuple(
-                hidden.view(batch, time, -1, seq_len, seq_len)
-                for hidden in vision_outputs.attentions
-            )
-            if vision_outputs.attentions is not None
-            else None
-        )
-        if return_dict:
-            return BaseModelOutputWithPooling(
-                last_hidden_state=last_hidden_state,
-                pooler_output=pooler_output,
-                hidden_states=hidden_states,
-                attentions=attentions,
-            )
-        return (last_hidden_state, pooler_output, hidden_states, attentions)
