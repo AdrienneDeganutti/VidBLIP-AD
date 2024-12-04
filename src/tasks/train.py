596f6f71fec3af42f354dee50c77562b28cd92bc
Adrienne Deganutti <ad01355@surrey.ac.uk>
2024-12-04 17:11:38 +0000

    add checkpointing

--- src/tasks/train.py
+++ src/tasks/train.py
@@ -1,6 +1,9 @@
+import os
 import torch
+import psutil
 import wandb
 import time
+from os.path import join
 from datetime import timedelta
 from torch.optim import AdamW
 from torch.optim.lr_scheduler import CosineAnnealingLR
@@ -9,18 +12,34 @@ from src.tasks.eval import Evaluation
 from src.utils.logger import LOGGER as logger
 
 
+def print_cpu_memory():
+    process = psutil.Process(os.getpid())
+    memory_info = process.memory_info()
+    print(f"CPU memory used: {memory_info.rss / 1024 ** 2:.2f} MB")  # rss is the resident set size
+
+def print_gpu_memory():
+    if torch.cuda.is_available():
+        gpu_memory_allocated = torch.cuda.memory_allocated() / 1024 ** 2  # Convert bytes to MB
+        gpu_memory_reserved = torch.cuda.memory_reserved() / 1024 ** 2  # Convert bytes to MB
+        print(f"GPU memory allocated: {gpu_memory_allocated:.2f} MB")
+        print(f"GPU memory reserved: {gpu_memory_reserved:.2f} MB")
+    else:
+        print("No GPU available.")
+
+
 class Train:
-    def __init__(self, training_args, model, processor, val_dataloader, train_dataloader):
+    def __init__(self, training_args, logging_args, model, processor, val_dataloader, train_dataloader):
         self.args = training_args
+        self.logging = logging_args
         self.model = model
         self.processor = processor 
         self.val_dataloader = val_dataloader
         self.train_dataloader = train_dataloader
-        self.evaluation = Evaluation(self.args, self.model, self.processor, self.val_dataloader, self.train_dataloader)
+        self.evaluation = Evaluation(self.args, self.logging, self.model, self.processor, self.val_dataloader, self.train_dataloader)
 
 
     def mixed_precision_init(self):
-        # Optimizer and Scheduler
+
         optimizer = AdamW(
             filter(lambda p: p.requires_grad, self.model.parameters()),
             lr=self.args.learning_rate,
@@ -29,13 +48,23 @@ class Train:
         num_training_steps = len(self.train_dataloader) * self.args.num_train_epochs
         scheduler = CosineAnnealingLR(optimizer, T_max=num_training_steps)
 
-        return optimizer, num_training_steps, scheduler
+        return optimizer, scheduler
+    
+
+    def save_model(self, epoch):
+        checkpoint_dir = join(self.args.output_dir, f'epoch_{epoch}')
+        os.makedirs(checkpoint_dir, exist_ok=True)
+
+        self.model.save_pretrained(checkpoint_dir)
+        self.processor.save_pretrained(checkpoint_dir)
+
+        return
 
 
     def train_epoch(self, optimizer, scheduler, epoch):
 
         self.model.train()
-        total_loss = 0.0
+        epoch_loss = 0.0
 
         if self.args.include_for_metrics:
             #total_metrics = {name: 0.0 for name in ['Bleu_1', 'Bleu_2', 'Bleu_3', 'Bleu_4', 'ROUGE_L', 'METEOR', 'CIDEr']}
@@ -58,8 +87,8 @@ class Train:
                 labels=labels
             )
             loss = outputs.loss
-            total_loss += loss.item()
-            wandb.log({"train_batch_loss": loss})
+            epoch_loss += loss.item()
+            wandb.log({"training_loss_step": loss}) if self.logging.wandb_log else None
 
             loss.backward()
             optimizer.step()
@@ -73,49 +102,40 @@ class Train:
                 wandb.log(log_metrics)
                 batch_count += 1
 
-        # Compute average loss
-        avg_loss = total_loss / len(self.train_dataloader)
-        return avg_loss
+        # Compute average loss over the full epoch
+        avg_loss = epoch_loss / len(self.train_dataloader)
+        logger.info(f"Training Loss: {avg_loss:.4f}")
+        wandb.log({"training_loss_epoch": avg_loss}) if self.logging.wandb_log else None
+        
+        return
 
 
     def train(self):
+        optimizer, scheduler = self.mixed_precision_init()
 
-        # Optimizer and Scheduler
-        optimizer, num_training_steps, scheduler = self.mixed_precision_init()
-
-        # WandB initialization
-        wandb.login() 
-        wandb.init(project="blip2-ad", name="Debugging",
-               config=vars(self.args))
+        if self.logging.wandb_log:
+            wandb.login() 
+            wandb.init(project="VidBLIP-AD", name=self.logging.wandb_project,
+                        config=vars(self.args))
     
         start_training_time = time.time()
 
-        # Training Loop
         for epoch in range(self.args.num_train_epochs):
+            
+            logger.info(f"Epoch {epoch}/{self.args.num_train_epochs}")
+            self.train_epoch(optimizer, scheduler, epoch)
+            self.evaluation.evaluate(self.args.output_dir, epoch)
 
-            #if epoch == 0 and self.args.eval_on_start and self.args.do_eval:
-                #logger.info('Performing initial validation at epoch 0.')
-                #epoch_0_val_loss, epoch_0_val_acc = self.evaluation.evaluate(self.args.output_dir, epoch)
-                #logger.info(f"Epoch 0 Validation Accuracy: {epoch_0_val_acc}")
-                #wandb.log({"validation_epoch_loss": epoch_0_val_loss})
-
-            logger.info(f"Epoch {epoch + 1}/{self.args.num_train_epochs}")
-
-            avg_train_loss = self.train_epoch(optimizer, scheduler, epoch)
-            logger.info(f"Train Loss: {avg_train_loss:.4f}")
-            wandb.log({"train_epoch_loss": avg_train_loss})
-
-            avg_val_loss = self.evaluation.evaluate(self.args.output_dir, epoch)    #Perform validation (loss-only) for every batch
-            wandb.log({"validation_epoch_loss": avg_val_loss})
+            if epoch != 0 and epoch % self.logging.checkpoint_epoch == 0:
+                self.save_model(epoch)
+                logger.info(f"Model and processor saved to {self.args.output_dir}epoch_{epoch}.")
     
         total_training_time = time.time() - start_training_time
         total_time_str = str(timedelta(seconds=total_training_time))
         logger.info(f'Total training time: {total_time_str} ({(total_training_time / self.args.max_iter):.4f} s / iter)')
 
-        wandb.finish()
-
-
         # Save the final model
-        self.model.save_pretrained(self.args.output_dir)
-        self.processor.save_pretrained(self.args.output_dir)
-        print("Model and processor saved!")
+        self.save_model(epoch)
+        logger.info("Model and processor saved!")
+
+        wandb.finish() if self.logging.wandb_log else None
